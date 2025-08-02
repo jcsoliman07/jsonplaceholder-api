13933f0dd788e6211d18dabd8330a4dcfe018306
Jomar <jomarsoliman01@gmail.com>
2025-08-03 01:02:49 +0800

    Create a group for User controller methods

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+});
