b878bfcd31e849fbc24805f101e5003e47c7e86a
Jomar <jomarsoliman01@gmail.com>
2025-08-03 00:07:12 +0800

    Create a table scheme for comments

--- database/migrations/2025_08_02_154224_create_comments_table.php
+++ database/migrations/2025_08_02_154224_create_comments_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('email');
+            $table->text('body');
             $table->timestamps();
         });
     }
