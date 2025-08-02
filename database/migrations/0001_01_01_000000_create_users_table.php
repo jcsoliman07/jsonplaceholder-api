eec2c34fba969b628ab2275ae371323e444ad938
Jomar <jomarsoliman01@gmail.com>
2025-08-02 23:43:39 +0800

    Modify users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('website')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
