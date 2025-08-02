a5209a037a5dc1ad0a7100eb0d0e586b13c8af0d
Jomar <jomarsoliman01@gmail.com>
2025-08-03 02:09:12 +0800

    Create and Add Custom Authentication driver usign username

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +22,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Auth::viaRequest('custom-auth', function ($request) 
+        {
+            return User::where('username', $request
+                        ->getUser())
+                        ->first();
+        });
     }
 }
