d2b40f48c618d9278678e9a72643f06e37f8fe7f
Jomar <jomarsoliman01@gmail.com>
2025-08-03 01:28:18 +0800

    Create and Add Fucntiosn for Comments

---
+++ app/Http/Controllers/Api/CommentController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    //
+
+    public function inde(): JsonResponse
+    {
+        $comments = Comment::all();
+
+        return response()->json(
+            [  
+                'status'    => 'success',
+                'data'      => $comments,
+            ]);
+    }
+
+    public function show (int $id): JsonResponse
+    {
+        $comment = Comment::find($id);
+
+        if  (!$comment)
+        {
+            return response()->json(
+            [  
+                'status'    => 'error',
+                'data'      => $comment,
+            ]);
+        }
+
+        return response()->json(
+            [
+                'status'    =>  'success',
+                'data'      =>  $comment,
+            ]);
+    }
+
+    public function indexByPost(int $postId): JsonResponse
+    {
+        $comments = Comment::where('post_id', $postId)->get();
+
+        return response()->json(
+            [
+                'status'    =>  'success',
+                'data'      =>  $comments,
+            ]);
+    }
+}
