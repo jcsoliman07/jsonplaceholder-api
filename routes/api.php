ab7f68d0500eee7f2c1832e105a0336dccaca0d1
Jomar <jomarsoliman01@gmail.com>
2025-08-03 01:30:06 +0800

    Add Group Route for Post and Comments

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,28 @@
 <?php
 
+use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//Group Routes for Users
 Route::prefix('users')->group(function () {
     Route::get('/', [UserController::class, 'index']);
     Route::get('/{id}', [UserController::class, 'show']);
+});
+
+//Group Routes for Post
+Route::prefix('posts')->group(function () {
+    Route::get('/', [PostController::class, 'index']);
+    Route::get('/{id}', [PostController::class, 'show']);
+    
+    // Nested comments under a specific post
+    Route::get('/{postId}/comments', [CommentController::class, 'indexByPost']);
+});
+
+//Group Routes for Comments
+Route::prefix('comments')->group(function () {
+    Route::get('/', [CommentController::class, 'index']);
+    Route::get('/{id}', [CommentController::class, 'show']);
 });
