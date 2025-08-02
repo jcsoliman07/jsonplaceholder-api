26bf5930fc8cf836a105f33b8f14999098ca7243
Jomar <jomarsoliman01@gmail.com>
2025-08-03 01:54:05 +0800

    Modify Rotue Group and make more cleaner

--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,48 @@
 <?php
 
+use App\Http\Controllers\Api\AlbumController;
 use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\PhotoController;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\TodoController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //Group Routes for Users
-Route::prefix('users')->group(function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::get('/{id}', [UserController::class, 'show']);
+Route::prefix('users')->controller(UserController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{id}', 'show');
 });
 
 //Group Routes for Post
-Route::prefix('posts')->group(function () {
-    Route::get('/', [PostController::class, 'index']);
-    Route::get('/{id}', [PostController::class, 'show']);
+Route::prefix('posts')->controller(PostController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{id}', 'show');
     
-    // Nested comments under a specific post
     Route::get('/{postId}/comments', [CommentController::class, 'indexByPost']);
 });
 
 //Group Routes for Comments
-Route::prefix('comments')->group(function () {
-    Route::get('/', [CommentController::class, 'index']);
-    Route::get('/{id}', [CommentController::class, 'show']);
+Route::prefix('comments')->controller(CommentController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{id}', 'show');
+});
+
+//Group Routes for Albums
+Route::prefix('albums')->controller(AlbumController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{id}', 'show');
+});
+
+//Group Routes for Photo
+Route::prefix('photos')->controller(PhotoController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{id}', 'show');
+});
+
+//Group Routes for Todo
+Route::prefix('todos')->controller(TodoController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{id}', 'show');
 });
