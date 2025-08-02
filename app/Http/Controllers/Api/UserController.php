def41b791758e26c3cb21ffc5a6658cc25802421
Jomar <jomarsoliman01@gmail.com>
2025-08-03 00:59:46 +0800

    Create a controller for User

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+
+    public function index(): JsonResponse
+    {
+        //Fetch all Users with address and company
+        $users = User::with(['address', 'company'])->get();
+
+        return response()->json($users);
+    }
+
+    public function show($id): JsonResponse
+    {
+        //Fetch specific user by Id
+        $user = User::with(['address', 'company'])->find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json($user);
+    }
+
+}
